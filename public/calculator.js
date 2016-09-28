1372ed8b72614fab1adb6a365d80e71ff6de8600
郑昊 <zhenghao@zhenghaodeMacBook-Air.local>
2016-09-28 00:00:36 -0700

    add grade function

--- public/calculator.js
+++ public/calculator.js
@@ -59,5 +59,47 @@ function weighted(){
   var output = (sum / weightsum) * 100;
   output = Math.round(output*100) / 100;
   document.getElementById('show').innerHTML = "Weighted: " + output + "%";
+  return output;
+}
+
+function grade(){
+  var grade = weighted();
+  if (grade >=95) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is A+ --- Great job!";
+  }
+  else if (grade >= 90 && grade < 95) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is A --- Nice work!";
+  }
+  else if (grade >= 85 && grade < 90) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is A- --- Pretty good!";
+  }
+  else if (grade >= 80 && grade < 85) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is B+ ";
+  }
+  else if (grade >= 75 && grade < 80) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is B";
+  }
+  else if (grade >= 70 && grade < 75) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is B-";
+  }
+  else if (grade >= 65 && grade < 70) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is C+";
+  }
+  else if (grade >= 60 && grade < 65) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is C";
+  }
+  else if (grade >= 55 && grade < 60) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is C-";
+  }
+  else if (grade >= 50 && grade < 55) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is D";
+  }
+  else if (grade < 50) {
+    document.getElementById('showgrade').innerHTML = "The letter grade is F ";
+    
+  }
+  else{
+    document.getElementById('showgrade').innerHTML = "Wrong input ";
+  }
 }
 
